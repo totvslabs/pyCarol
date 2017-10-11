d4e9156ba4dc4ef97b1d6902eec8f5e48300df2f
Rafael <rafarui@gmail.com>
2017-10-11 08:54:04 -0300

    refactor files

--- __init__.py
+++ __init__.py
@@ -1,9 +1,1 @@
 __version__ = '0.1'
-
-
-from .loginCarol import *
-from .stagingCarol import *
-from .utils import *
-from .schemaGenerator import *
-from .queriesCarol import *
-from .sendSchema import *
