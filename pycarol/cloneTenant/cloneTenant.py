ff391ddb99ad6e5f9b6682eff58b7c3190cc867b
Rafael <rafarui@gmail.com>
2017-12-01 15:36:41 -0200

    cloneTenant

--- pycarol/cloneTenant/cloneTenant.py
+++ pycarol/cloneTenant/cloneTenant.py
@@ -125,8 +125,81 @@ class cloneTenant(object):
                     self.stag_mapp_to_use[connectorName].append({"schema": aux_schema})
 
 
-    def copyConnectors(self, staging_list,  copy_mapping=True, overwrite=False):
-        pass
+    def copyConnectors(self, conectors_map, map_type = 'connectorId', copy_mapping=True, overwrite=False):
+
+        if map_type == 'connectorId':
+            map_type = 'mdmId'
+        elif map_type == 'name':
+            map_type = 'mdmName'
+        else:
+            raise('values should be connectorId or name')
+
+
+        conn = appl.connectorsCarol(self.token_from)
+        conn.getAll(includeMappings=True)
+        conn_to_create = conn.connectors
+
+        conn_id = {}
+
+        stag = stg.stagingSchema(self.token_from)
+        self.stag_mapp_to_use = defaultdict(list)
+
+        for connector, staging in conectors_map.items():
+            #for connector in conn_to_create:
+
+            current_connector = connector['mdmId']
+            conn.connectorStats(current_connector)
+            conn_stats = conn.connectorsStats_
+
+            connectorName = connector.get('mdmName', None)
+            connectorLabel = connector.get('mdmLabel', None)
+            if connectorLabel:
+                connectorLabel = connectorLabel['en-US']
+            else:
+                connectorLabel = None
+            groupName = connector.get('mdmGroupName', None)
+
+            conn_to = appl.connectorsCarol(self.token_to)
+            conn_to.createConnector(connectorName, connectorLabel, groupName, overwrite=overwrite)
+            conn_id.update({connectorName: conn_to.connectorId})
+            self.token_to.newToken(connectorId=conn_to.connectorId)
+
+            for schema_name in conn_stats.get(current_connector):
+                stag.getSchema(schema_name, connector.get('mdmId'))
+
+                aux_schema = stag.schema
+                aux_schema.pop('mdmTenantId')
+                aux_schema.pop('mdmStagingApplicationId')
+                aux_schema.pop('mdmId')
+                aux_schema.pop('mdmCreated')
+                aux_schema.pop('mdmLastUpdated')
+
+                stg_to = stg.stagingSchema(self.token_to)
+                stg_to.sendSchema(fields_dict=aux_schema, connectorId=conn_id.get(connectorName),
+                                  overwrite=overwrite)
+
+                if copy_mapping:
+                    mapping_fields = connector.get('mdmEntityMappings', None).get(schema_name)
+                    if mapping_fields is not None:
+                        mapping_fields.pop('mdmTenantId')
+                        entityMappingsId = mapping_fields.pop('mdmId')
+                        entitySpace = mapping_fields.get('mdmEntitySpace')
+                        mapping_fields.pop('mdmCreated')
+                        mapping_fields.pop('mdmLastUpdated')
+                        connectorId = mapping_fields.pop('mdmApplicationId')
+                        mappings_to_get = etm.entityMapping(self.token_from)
+                        mappings_to_get.getSnapshot(connectorId, entityMappingsId, entitySpace)
+                        _, aux_map = mappings_to_get.snap.popitem()
+                        mapping_to = etm.entityMapping(self.token_to)
+                        mapping_to.createFromSnnapshot(aux_map, conn_id.get(connectorName), overwrite=overwrite)
+                        self.stag_mapp_to_use[connectorName].append({"schema": aux_schema, "mapping": aux_map})
+                    else:
+                        self.stag_mapp_to_use[connectorName].append({"schema": aux_schema})
+                else:
+                    self.stag_mapp_to_use[connectorName].append({"schema": aux_schema})
+
+
+
 
 
 
