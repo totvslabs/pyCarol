7c4b5b2676fc18db1a87328dcb84378b37316212
Rafael <rafarui@gmail.com>
2018-04-04 16:02:37 -0300

    fix dict to list

---
+++ pycarol/tenantsCarol.py
@@ -0,0 +1,33 @@
+import json
+import requests
+from . import namedQueryCarol as nq
+#from . import utils
+import re
+
+
+
+class tenantInfo:
+    def __init__(self,token_object):
+        self.dev = token_object.dev
+        self.token_object = token_object
+        self.headers = self.token_object.headers_to_use
+
+    def getInfo(self,domain= None):
+
+        if domain is None:
+            domain = self.token_object.domain
+        _true = True
+        url = "https://{}.carol.ai{}/api/v2/tenants/domain/{}".format(self.token_object.domain,self.dev,domain)
+        while _true:
+            self.response = requests.request("GET", url, headers=self.headers)
+            if not self.response.ok:
+                # error handler for token
+                if self.response.reason == 'Unauthorized':
+                    self.token_object.refreshToken()
+                    self.headers = {'Authorization': self.token_object.access_token, 'Content-Type': 'application/json'}
+                    continue
+                raise Exception(self.response.text)
+            self.response.encoding = 'utf8'
+            self.tenantInfo = json.loads(self.response.text)
+            _true =False
+        return self.tenantInfo
--- pycarol/utils/utils.py
+++ pycarol/utils/utils.py
@@ -29,27 +29,4 @@ def scrub(obj, bad):
         pass
 
 
-class tenantsCarol:
-    def __init__(self,token_object):
-        self.dev = token_object.dev
-        self.token_object = token_object
-        self.headers = self.token_object.headers_to_use
-
-    def getInfo(self,domain):
-        _true = True
-        url = "https://{}.carol.ai{}/api/v2/tenants/domain/{}".format(self.token_object.domain,self.dev,domain)
-        while _true:
-            self.response = requests.request("GET", url, headers=self.headers)
-            if not self.response.ok:
-                # error handler for token
-                if self.response.reason == 'Unauthorized':
-                    self.token_object.refreshToken()
-                    self.headers = {'Authorization': self.token_object.access_token, 'Content-Type': 'application/json'}
-                    continue
-                raise Exception(self.response.text)
-            self.response.encoding = 'utf8'
-            self.tenantInfo = json.loads(self.response.text)
-            _true =False
-
-
 
