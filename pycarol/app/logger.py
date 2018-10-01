20e87677c7f657e38d71007d88116d7238015867
rafarui <rafarui@gmail.com>
2018-10-01 17:42:53 -0300

    change logger.py

--- pycarol/app/logger.py
+++ pycarol/app/logger.py
@@ -1,10 +1,18 @@
 import os
 import logging
 
-class Logger():
-    """ It implements google.cloud.logging and return a logging object in order to send logs to the Stackdriver.
-    If the $GOOGLE_APPLICATION_CREDENTIALS is not defined, this class will provide a default 'logging' and the
-    logs will not be send to Stackdriver.
+
+class CarolHandler(logging.Handler):
+    """ Handler to organize Carol logging
+    This implementation send logs to the Google Stackdriver.
+
+    Required OS Environment variables:
+        * GOOGLE_APPLICATION_CREDENTIALS
+        * CAROLPROCESSTYPE
+        * CAROLONLINENAME | CAROLBATCHNAME
+        * CAROLTENANT
+        * CAROLAPPNAME
+        * CAROLAPPVERSION
 
     Usage:
         from logger import Logger
@@ -12,34 +20,31 @@ class Logger():
         logger.log.warning('It`s a warning message')
         logger.log.error('It`s an error message')
         logger.log.critical('It`s a critical message')
-
         ## It's possible add another handler
         # logger.log.addHandler(<handle obj>)
     """
 
-    def __init__(self, name=None):
+    def __init__(self, level=logging.NOTSET):
+        super().__init__(level)
+        process_type = os.environ.get('CAROLPROCESSTYPE', '')
+        tenant = os.environ.get('CAROLTENANT', '')
+        app_name = os.environ.get('CAROLAPPNAME', '')
+        app_version = os.environ.get('CAROLAPPVERSION', '')
+        process_name = ''
+        if process_type.lower() == 'online':
+            process_name = os.environ.get('CAROLONLINENAME', '')
+        elif process_type.lower() == 'batch':
+            process_name = os.environ.get('CAROLBATCHNAME', '')
+        self.formatter = logging.Formatter('{} > {} > {} > {} > {} > %(message)s'.format(
+            tenant, app_name, app_version, process_type, process_name))
+
         if os.environ.get('GOOGLE_APPLICATION_CREDENTIALS'):
-            process_type = os.environ.get('CAROLPROCESSTYPE', '')
-
-            process_name = ''
-            if process_type.lower() == 'online':
-                process_name = os.environ.get('CAROLONLINENAME', '')
-            elif process_type.lower() == 'batch':
-                process_name = os.environ.get('CAROLBATCHNAME', '')
-
-            tenant = os.environ.get('CAROLTENANT', '')
-            app_name = os.environ.get('CAROLAPPNAME', '')
-            app_version = os.environ.get('CAROLAPPVERSION', '')
-
             from google.cloud import logging as google_logging
             client = google_logging.Client()
-
-            formatter = logging.Formatter('{} > {} > {} > {} > {} > %(message)s'.format(tenant, app_name, app_version, process_type, process_name))
-            handler = client.get_default_handler()
-            handler.setFormatter(formatter)
-
-            self.log = logging.getLogger(name)
-            self.log.addHandler(handler)
-
+            self.handler = client.get_default_handler()
         else:
-            self.log = logging.getLogger(name)
+            raise ValueError('Tried to create a CarolHandler but no credentials for a service were found.')
+        self.handler.setFormatter(self.formatter)
+
+    def emit(self, record):
+        return self.handler.emit(record)
