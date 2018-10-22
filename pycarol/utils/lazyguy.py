01c9c2cc63ce4e901b77962774804cd2bcc836ef
rafarui <rafarui@gmail.com>
2018-10-22 16:11:35 -0300

    example

--- pycarol/utils/lazyguy.py
+++ pycarol/utils/lazyguy.py
@@ -7,13 +7,62 @@ import json
 
 
 class lazeDM(object):
+
+    """
+    This class will create a Datamodel from a python dictionary. Today, it is not allowed nested fileds in the dictionary.
+
+    Ex:
+    ```python
+    from pycarol.auth.PwdAuth import PwdAuth
+    from pycarol.auth.ApiKeyAuth import ApiKeyAuth
+    from pycarol.carol import Carol
+
+    from pycarol.utils import lazyguy
+
+    json_sample = {'atende_sus': 'Sim',
+                   'estabelecimento': 'Hospital Raimundo Chaar',
+                   'existente': '42',
+                   'tipo_do_estabelecimento': '05 - Hospital Geral',
+                   'tipo_do_estabelecimento_resumo': 'Hospitais',
+                   'uf': 'AC'}
+
+    login = Carol('foo_tenant, 'foo', auth=PwdAuth('foo@totvs.com.br', 'foo123'))
+    la = lazyguy.lazeDM(login)
+    la.start(json_sample, dm_name='hfair', profile_title='estabelecimento', publish=True, overwrite=True)
+    ```
+
+    """
+
+
     def __init__(self, carol):
+        """
+
+        :param carol: Carol object
+            Carol object.
+        """
         self.carol = carol
 
     def start(self, json_sample, dm_name, publish=True, profile_title=None, overwrite=True, vertical_ids=None,
               vertical_names='retail', entity_template_type_ids=None,
               entity_template_type_names='product', dm_label=None,
               group_name='Others', transaction_dm=False):
+
+        """
+
+        :param json_sample:
+        :param dm_name:
+        :param publish:
+        :param profile_title:
+        :param overwrite:
+        :param vertical_ids:
+        :param vertical_names:
+        :param entity_template_type_ids:
+        :param entity_template_type_names:
+        :param dm_label:
+        :param group_name:
+        :param transaction_dm:
+        :return:
+        """
 
         if publish:
             assert profile_title in json_sample
