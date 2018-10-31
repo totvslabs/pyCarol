9286a93fff6fcd87653c77502702985e07761c83
rafarui <rafarui@gmail.com>
2018-10-31 18:11:08 -0300

    export stats

--- pycarol/tenant.py
+++ pycarol/tenant.py
@@ -8,10 +8,24 @@ class Tenant:
         self.carol = carol
 
     def get_tenant_by_domain(self, domain):
+        """
+        Get tenant information.
+
+        :param domain: `str`
+            Tenant name
+        :return:
+            dict with the information about the tenant.
+        """
         return self.carol.call_api('v2/tenants/domain/{}'.format(domain), auth=False)
 
 
     def _get_staging(self):
+        """
+        Get export status for data models
+
+        :return: `dict`
+            dict with the information of which staging table is exporting its data.
+        """
 
         query = Query(self.carol, index_type='CONFIG', only_hits=False)
 
@@ -28,6 +42,12 @@ class Tenant:
 
 
     def _get_dm(self):
+        """
+        Get export status for data models
+
+        :return: `dict`
+            dict with the information of which data model is exporting its data.
+        """
 
         json_q  = Filter.Builder(key_prefix="")\
             .must(TYPE_FILTER(value="mdmEntityTemplateExport")).build().to_json()
@@ -50,6 +70,16 @@ class Tenant:
 
 
     def check_exports(self, staging=True, data_model=True):
+        """
+        Find all data models or stating tables that have export active.
+
+        :param staging: `bool`, default `True`
+            Get status for exports in staging.
+        :param data_model: `bool`, default `True`
+            Get status for exports in data models.
+        :return: `tuple`
+            staging stats, data model stats
+        """
 
         staging_results = None
         dm_results = None
