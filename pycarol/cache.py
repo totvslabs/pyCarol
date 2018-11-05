1e3af541a2c14f220593639c8baf268d4900e760
wmsouza <>
2018-11-05 02:14:46 -0500

    Cache

---
+++ pycarol/cache.py
@@ -0,0 +1,16 @@
+from pycarol.utils import KeySingleton
+import redis
+
+
+class Cache(metaclass=KeySingleton):
+    def __init__(self, carol):
+        self.carol = carol
+        self.r = redis.StrictRedis(host='104.196.240.161', port=6379, db=0)
+
+    def set(self, key, val):
+        key = '{}-{}-{}'.format(self.carol.tenant['mdmId'], self.carol.app_name, key)
+        self.r.set(key, val)
+
+    def get(self, key):
+        key = '{}-{}-{}'.format(self.carol.tenant['mdmId'], self.carol.app_name, key)
+        return self.r.get(key)
