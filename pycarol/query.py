bb562d7e93f9acb4f832d246397b2a6b529fd6db
rafarui <rafarui@gmail.com>
2018-12-05 15:09:44 -0200

    remove from websocket import create_connection

--- pycarol/query.py
+++ pycarol/query.py
@@ -1,5 +1,4 @@
 import json
-from websocket import create_connection
 import itertools
 from joblib import Parallel, delayed
 import dask
@@ -138,6 +137,7 @@ class Query:
         return self
 
     def _streamable_query_handler(self, callback=None):
+        from websocket import create_connection
         if not self.offset == 0:
             raise ValueError('It is not possible to use offset when using streaming')
 
