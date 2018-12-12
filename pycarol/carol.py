dd8e619ac41bdeefa909734820a13a16ed803c72
rafarui <rafarui@gmail.com>
2018-12-12 15:53:00 -0300

    fix login

--- pycarol/carol.py
+++ pycarol/carol.py
@@ -32,50 +32,55 @@ class Carol:
         """
 
 
-        settings = dict()
-        if os.path.isfile('app_config.json'):
-            with open('app_config.json', 'r') as f:
-                settings = json.load(f)
+        if auth is None and domain is None:
+            settings = dict()
+            if os.path.isfile('app_config.json'):
+                with open('app_config.json', 'r') as f:
+                    settings = json.load(f)
 
-            if domain is None:
-                domain = settings.get('domain', os.getenv('CAROLTENANT'))
-            if app_name is None:
-                app_name = settings.get('app_name', os.getenv('CAROLAPPNAME'))
+                if domain is None:
+                    domain = settings.get('domain', os.getenv('CAROLTENANT'))
+                if app_name is None:
+                    app_name = settings.get('app_name', os.getenv('CAROLAPPNAME'))
 
-            app_config = settings.get(domain)
-            if app_config is not None:
-                if auth is None:
-                    auth_token = app_config.get('oauth_token')
-                    if auth_token is not None:
-                        auth = ApiKeyAuth(auth_token)
-                    else:
-                        auth_user = app_config.get('auth_user')
-                        auth_pwd = app_config.get('auth_pwd')
-                        if auth_user is not None and auth_pwd is not None:
-                            auth = PwdAuth(auth_user, auth_pwd)
+                app_config = settings.get(domain)
+                if app_config is not None:
+                    if auth is None:
+                        auth_token = app_config.get('oauth_token')
+                        if auth_token is not None:
+                            auth = ApiKeyAuth(auth_token)
                         else:
-                            auth_token = os.getenv('CAROLAPPOAUTH')
-                            if auth_token is not None:
-                                auth = ApiKeyAuth(auth_token)
+                            auth_user = app_config.get('auth_user')
+                            auth_pwd = app_config.get('auth_pwd')
+                            if auth_user is not None and auth_pwd is not None:
+                                auth = PwdAuth(auth_user, auth_pwd)
                             else:
-                                auth_user = os.getenv('CAROLUSER')
-                                auth_pwd = os.getenv('CAROLPWD')
-                                if auth_user is not None and auth_pwd is not None:
-                                    auth = PwdAuth(auth_user, auth_pwd)
+                                auth_token = os.getenv('CAROLAPPOAUTH')
+                                if auth_token is not None:
+                                    auth = ApiKeyAuth(auth_token)
+                                else:
+                                    auth_user = os.getenv('CAROLUSER')
+                                    auth_pwd = os.getenv('CAROLPWD')
+                                    if auth_user is not None and auth_pwd is not None:
+                                        auth = PwdAuth(auth_user, auth_pwd)
 
-                if connector_id is None:
-                    connector_id = app_config.get('connector_id', os.getenv('CAROLCONNECTORID', '0a0829172fc2433c9aa26460c31b78f0'))
+                    if connector_id is None:
+                        connector_id = app_config.get('connector_id', os.getenv('CAROLCONNECTORID', '0a0829172fc2433c9aa26460c31b78f0'))
 
-        else: # env login
-            domain = os.getenv('CAROLTENANT')
-            app_name = os.getenv('CAROLAPPNAME')
-            auth_token = os.getenv('CAROLAPPOAUTH')
-            connector_id = os.getenv('CAROLCONNECTORID')
-            assert (domain and app_name and auth and connector_id,
-                    "One of the following env variables are missing:\n"+
-                    "CAROLTENANT:{}\nCAROLAPPNAME{}\nCAROLAPPOAUTH:{}\nCAROLCONNECTORID{}\n".format(
-                    domain,app_name,auth,connector_id))
-            auth = ApiKeyAuth(auth_token)
+            else: # env login
+                domain = os.getenv('CAROLTENANT')
+                app_name = os.getenv('CAROLAPPNAME')
+                auth_token = os.getenv('CAROLAPPOAUTH')
+                connector_id = os.getenv('CAROLCONNECTORID')
+                assert (domain and app_name and auth and connector_id,
+                        "One of the following env variables are missing:\n"+
+                        "CAROLTENANT:{}\nCAROLAPPNAME{}\nCAROLAPPOAUTH:{}\nCAROLCONNECTORID{}\n".format(
+                        domain,app_name,auth,connector_id))
+                auth = ApiKeyAuth(auth_token)
+
+
+        if connector_id is None:
+            connector_id =  '0a0829172fc2433c9aa26460c31b78f0'
 
         if domain is None or app_name is None or auth is None:
             raise ValueError("domain, app_name and auth must be specified as parameters, in the app_config.json file " +
