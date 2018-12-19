ea2489190ace9728f1a8208af8720782e1589f88
gabriel.magalhaes <gabriel.magalhaes@totvs.com.br>
2018-12-18 22:29:15 -0200

    Adding to init

--- pycarol/pipeline/__init__.py
+++ pycarol/pipeline/__init__.py
@@ -1,3 +1,3 @@
 from .data_model import DataModel
 from .field import Field
-from .luigi import StagingIngestion, SettingsDefinition, set_parameters, Ingestion, Parameter
+from .luigi import StagingIngestion, SettingsDefinition, set_parameters, Ingestion, Parameter, DictParameter
