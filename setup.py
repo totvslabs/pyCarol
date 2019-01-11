6f5f5211ac506249a73e3b0bac0d459e4962af24
gabriel.magalhaes <gabriel.magalhaes@totvs.com.br>
2019-01-11 15:24:19 -0200

    Setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
-# with open('requirements.txt', 'r') as req_file:
-#     install_requires = req_file.read()
+with open('requirements.txt', 'r') as req_file:
+    install_requires = req_file.read()
 
 setup(
     name='pycarol',
@@ -9,5 +9,5 @@ setup(
     description='Carol Python API',
     packages=find_packages(exclude=['docs', 'doc']),
     maintainer='TOTVS Labs',
-    # install_requires= install_requires.splitlines()
+    install_requires= install_requires.splitlines()
 )
