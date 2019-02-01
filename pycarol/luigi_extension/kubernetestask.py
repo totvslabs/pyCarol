623e44fda1bd997c02e0a52f48471da97a3b8ff0
rafarui <rafarui@gmail.com>
2019-02-01 15:14:59 -0800

    _file_id for k8s name

--- pycarol/luigi_extension/kubernetestask.py
+++ pycarol/luigi_extension/kubernetestask.py
@@ -94,8 +94,8 @@ class EasyKubernetesTask(EasyDockerTask):
         #Name cannot have '.', also have to be less de 63 characters
         #TODO: Is this the best way? this will be the name of the task+package it is in.
 
-        return self._file_id().lower()
-        #return '-'.join(self.get_task_family().lower().split('.')[-2:])
+        #return self._file_id().lower()
+        return '-'.join(self.get_task_family().lower().split('.')[-2:])
 
     @property
     def labels(self):
