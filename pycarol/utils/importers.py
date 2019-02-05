36b00859e5b1330271c634db1664480e7ce24fe9
Thiago Henrique Rizzi Donato <t.dunkin@gmail.com>
2019-02-05 16:26:00 +0000

    Debug pyArrow backend in order to verify in column parameter is empty

--- pycarol/utils/importers.py
+++ pycarol/utils/importers.py
@@ -191,10 +191,15 @@ def _import_pyarrow(s3, tenant_id, dm_name=None,connector_id=None, columns=None,
                 if count >=max_hits:
                     df_list[i] = df_list[i].iloc[:max_hits-count_old]
                     break
-            df_temp = df_temp.drop_duplicates(
-                subset=columns,
-                keep='first',
-            )
+            if columns:
+                df_temp = df_temp.drop_duplicates(
+                    subset=columns,
+                    keep='first',
+                )
+            else:
+                df_temp = df_temp.drop_duplicates(
+                    keep='first',
+                )                
             df_list[ii] = pa.RecordBatch.from_pandas(df_temp)                    
             ii = ii + 1
         if not df_list:
