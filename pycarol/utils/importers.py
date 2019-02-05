ea04830de1596fa5143741c8a2c647428eb56760
Thiago Henrique Rizzi Donato <t.dunkin@gmail.com>
2019-02-05 20:26:31 +0000

    Rollback _import_pandas importer modif

--- pycarol/utils/importers.py
+++ pycarol/utils/importers.py
@@ -125,25 +125,7 @@ def _import_pandas(s3, tenant_id, dm_name=None,connector_id=None, columns=None,
             obj=s3.Object(__BUCKET_NAME__, file)
             buffer = io.BytesIO()
             obj.download_fileobj(buffer)
-            if columns:
-                df_list.append(
-                    pd.read_parquet(
-                        buffer,
-                        columns=columns,
-                    ).drop_duplicates(
-                        subset=columns,
-                        keep='first',
-                    )
-                )
-            else:
-                df_list.append(
-                    pd.read_parquet(
-                        buffer,
-                        columns=columns,
-                    ).drop_duplicates(
-                        keep='first',
-                    )
-                )                
+            df_list.append(pd.read_parquet(buffer,columns=columns))               
             if max_hits is not None:
                 count_old = count
                 count += len(df_list[i])
