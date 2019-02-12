e517adcdbf63150e3acd45f433fefb1a9a8b7cb9
Gabriel Magalhaes <gabriel.magalhaes@totvs.com.br>
2019-02-12 17:28:10 -0200

    First draft

---
+++ pycarol/luigi_extension/visualization.py
@@ -0,0 +1,17 @@
+
+
+class Visualization:
+    """ Main class to control Tasks visualizations
+    """
+
+    def __init__(self, task=None):
+        self._task = task
+
+    def set_display_option(self, display_option):
+        """ Is this visualization being rendered on a jupyter or as file?
+
+        :param display_option:
+        :return:
+        """
+        # TODO
+        pass
