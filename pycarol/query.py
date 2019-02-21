ea43000966c18454f84a8e70ee00136e8b3561d6
rafarui <rafarui@gmail.com>
2019-02-21 12:11:38 -0800

    parquery mdmStagingRecord

--- pycarol/query.py
+++ pycarol/query.py
@@ -470,7 +470,7 @@ class ParQuery:
             self.fields_to_get = fields
             self.fields=None
             self.only_hits=False
-            self.mdmKey = 'mdmStagingRecord'
+            self.mdmKey = 'mdmCounterForEntity'
         elif get_staging_from_golden:
             self.index_type = 'MASTER'
             self.datamodel_name = f"{datamodel_name}Master"
