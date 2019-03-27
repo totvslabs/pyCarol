76555535e317a56bdd40d50b61c527a722b5ae60
rafarui <rafarui@gmail.com>
2019-03-27 08:55:58 -0700

    connector name in create_schema

--- pycarol/__init__.py
+++ pycarol/__init__.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 
-__version__ = '2.10'
+__version__ = '2.11'
 
 __BUCKET_NAME__= 'carol-internal'
 __TEMP_STORAGE__ = os.path.join(tempfile.gettempdir(),'carolina/cache')
