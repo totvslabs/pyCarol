c94193ed7e1739bab298f227ff9b9ae56aeebc65
Giovani Zamboni <gzamboni@gmail.com>
2019-04-30 01:20:26 -0300

    gitignore, makefiles, ci/cd

---
+++ Makefile
@@ -0,0 +1,28 @@
+.PHONY: help clean dev docs package test
+
+help:
+	@echo "This project assumes that an active Python virtualenv is present."
+	@echo "The following make targets are available:"
+	@echo "	 dev 	install all deps for dev env"
+	@echo "  docs	create pydocs for all relveant modules"
+	@echo "	 test	run all tests with coverage"
+
+clean:
+	rm -rf dist/*
+
+dev:
+	pip install -r dev-requirements.txt
+	pip install -e .
+
+docs:
+	$(MAKE) -C docs html
+
+package:
+	python setup.py sdist
+	python setup.py bdist_wheel
+
+test:
+	# coverage --collect-only run -m unittest discover
+	echo "This is a temporary step. CHECK THOSES TESTS"
+	nosetests --with-coverage3 --collect-only
+	# coverage html
--- setup.py
+++ setup.py
@@ -1,16 +1,38 @@
+from distutils.core import setup
 from setuptools import setup, find_packages
 
 
 with open('requirements.txt', 'r') as req_file:
     install_requires = req_file.read()
 
+# setup(
+#     name='pycarol',
+#     version='2.12',
+#     description='Carol Python API',
+#     packages=find_packages(exclude=['docs', 'doc']),
+#     maintainer='TOTVS Labs',
+#     install_requires=install_requires.splitlines()
+# )
+
 setup(
     name='pycarol',
-    version='2.12',
-    description='Carol Python API',
     packages=find_packages(exclude=['docs', 'doc']),
+    version='2.12',
+    license='TOTVS',
+    description='Carol Python API and Tools',
+    author='TotvsLabs',
     maintainer='TOTVS Labs',
-    install_requires=install_requires.splitlines()
+    author_email='ops@totvslabs.com',
+    url='https://www.carol.ai/',
+    keywords=['Totvs', 'Carol.ai', 'AI'],
+    install_requires=install_requires.splitlines(),
+    classifiers=[
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Development Status :: 5 - Production/Stable',
+        # Define that your audience are developers
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3.6',
+    ],
 )
-
-
