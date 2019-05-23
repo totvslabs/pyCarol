940bd19f23e32086455add460dfb6afc123aaef8
rafarui <rafarui@gmail.com>
2019-05-23 11:30:18 -0300

    print task

--- pycarol/__init__.py
+++ pycarol/__init__.py
@@ -1,7 +1,7 @@
 import os
 import tempfile
 
-__version__ = '2.12.2'
+__version__ = '2.12.3'
 
 __BUCKET_NAME__= 'carol-internal'
 __TEMP_STORAGE__ = os.path.join(tempfile.gettempdir(),'carolina/cache')
