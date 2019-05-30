59fc41b1a801f00df297b0a5ac70c5439790938d
rafarui <rafarui@gmail.com>
2019-05-30 17:14:31 -0300

    fix local dump parquet

--- pycarol/luigi_extension/targets.py
+++ pycarol/luigi_extension/targets.py
@@ -177,7 +177,7 @@ class ParquetTarget(CDSTarget):
 
     def dump_local(self, function_output):
         os.makedirs(os.path.dirname(self.path), exist_ok=True)
-        function_output.to_parquet(self.path, has_nulls='infer')
+        function_output.to_parquet(self.path)
 
 
 class KerasTarget(CDSTarget):
