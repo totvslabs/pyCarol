81b556cbc69d9e2b2550bb1fdb990a5efb259218
rafarui <rafarui@gmail.com>
2019-06-04 10:43:34 -0300

    fix spaces

--- pycarol/data_models/data_model_view.py
+++ pycarol/data_models/data_model_view.py
@@ -1,18 +1,11 @@
 import warnings
 import pandas as pd
 import json
-
-
-
 from ..utils.importers import _import_dask, _import_pandas
 from ..storage import Storage
 from ..query import Query
 from ..filter import TYPE_FILTER, Filter
 import itertools
-
-
-
-
 
 
 class DataModelView:
