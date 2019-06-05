a90d821b1f8cfc02444be90a59df31676e9c87c3
rafonseca <renanfonseca@gmail.com>
2019-06-05 18:32:52 +0200

    using importlib to get analyzed function context

--- pycarol/luigi_extension/hash_versioning.py
+++ pycarol/luigi_extension/hash_versioning.py
@@ -1,15 +1,17 @@
 import dis
+import importlib
 
 def _traverse_code(analyzed_function):
     inner_functions_set = set()
     instructions = list(dis.get_instructions(analyzed_function))
-
-
+    m = analyzed_function.__module__
+    imported = importlib.import_module(m)
     for ix, inst in enumerate(instructions):
         if 'CALL_FUNCTION' == inst.opname:
             called_function_name = instructions[ix - inst.arg - 1]
             function_name = called_function_name.argval
-            func = globals()[function_name]
+            func = getattr(imported, function_name)
+            print(function_name,func)
             if func not in code_set:
                 code_set.add(func)
                 inner_functions_set.add(func)
@@ -23,8 +25,8 @@ def _traverse_code(analyzed_function):
 def get_code_hash(func):
 
     if not hasattr(func, '__code__'):
-        raise TypeError('parameter passer to get_code_hash must be a function')
-
+        raise TypeError('parameter passed to get_code_hash must be a function')
+    global code_set
     code_set = set() # memoization set
     bytecode_list = _traverse_code(func)
     if isinstance(bytecode_list,list):
--- pycarol/luigi_extension/pytest/test_hash_versioning.py
+++ pycarol/luigi_extension/pytest/test_hash_versioning.py
@@ -3,7 +3,6 @@ from pytest import mark
 from ..hash_versioning import get_code_hash
 
 
-
 def a(x):
     return x + 5
 def b(z):
@@ -11,10 +10,29 @@ def b(z):
 def c(x):
     return x - 5
 
+def a_of_a(x):
+    return a(x)
+
+def a_of_b(x):
+    return b(x)
+
+def eternal_recursion_a(x):
+    return eternal_recursion_a(x)
+def eternal_recursion_b(x):
+    return eternal_recursion_b(x)
+def mutual_recursion_a(x):
+    return mutual_recursion_b(x)
+def mutual_recursion_b(x):
+    return mutual_recursion_a(x)
+
+
 equal_functions_list = [
     (a,a),
     (b,a),
-    (b,a),
+    (a,b),
+    (a_of_a,a_of_b),
+    (eternal_recursion_a,eternal_recursion_b),
+    (mutual_recursion_a,mutual_recursion_b),
 ]
 
 different_functions_list = [
