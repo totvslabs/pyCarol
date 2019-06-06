b9abea6c817f6d28044d733d7338c4cd81d241b3
rafonseca <renanfonseca@gmail.com>
2019-06-06 12:56:17 +0200

    support builtin functions. comment and reformat code

--- pycarol/luigi_extension/hash_versioning.py
+++ pycarol/luigi_extension/hash_versioning.py
@@ -1,35 +1,48 @@
 import dis
 import importlib
+import builtins
 
-def _traverse_code(analyzed_function):
-    inner_functions_set = set()
-    instructions = list(dis.get_instructions(analyzed_function))
-    m = analyzed_function.__module__
-    imported = importlib.import_module(m)
-    for ix, inst in enumerate(instructions):
-        if 'CALL_FUNCTION' == inst.opname:
-            called_function_name = instructions[ix - inst.arg - 1]
-            function_name = called_function_name.argval
-            func = getattr(imported, function_name)
-            print(function_name,func)
-            if func not in code_set:
-                code_set.add(func)
-                inner_functions_set.add(func)
-    code_list = [_traverse_code(f) for f in inner_functions_set]
-    code_list.append(analyzed_function.__code__.co_code)
-    if len(code_list) == 1:
-        return code_list[0]
-    else:
+
+def get_bytecode_tree(analyzed_function):
+    def _traverse_code(_analyzed_function):
+        nonlocal code_set
+        inner_functions_set = set()
+        if hasattr(builtins, _analyzed_function.__name__):
+            # dis cannot get instructions for builtins
+            # return function name instead of bytecode
+            return [_analyzed_function.__name__]
+        instructions = list(dis.get_instructions(_analyzed_function))
+
+        m = _analyzed_function.__module__
+        imported = importlib.import_module(m)
+        for ix, inst in enumerate(instructions):
+            if "CALL_FUNCTION" in inst.opname:  # call function instruction found
+                if "CALL_FUNCTION" == inst.opname:  # it is simple call function instruction
+                    # for this instruction, we can find the called function some instructions
+                    # above. we just need to skip bakcwards the number of arguments
+                    called_function_name = instructions[ix - inst.arg - 1]
+                else:
+                    raise (NotImplementedError, "{} instruction is not supported".format(inst.opname))
+                function_name = called_function_name.argval
+                # function_name can be found in Local, Enclosed, Global and Builtin Namespace
+                # Local and enclosed namespaces cannot be discovered in static analysis.
+                # So there is limited support to nested functions like this one.
+                try:
+                    func = getattr(imported, function_name)  # Module (Global) Namespace
+                except AttributeError:
+                    func = getattr(builtins, function_name)  # Builtin Namespace
+
+                if func not in code_set:
+                    code_set.add(func)
+                    inner_functions_set.add(func)
+        code_list = [_traverse_code(f) for f in inner_functions_set]
+        code_list.append(_analyzed_function.__code__.co_code)
         return code_list
 
-def get_code_hash(func):
+    if not hasattr(analyzed_function, '__code__'):
+        raise TypeError('argument should be a function')
 
-    if not hasattr(func, '__code__'):
-        raise TypeError('parameter passed to get_code_hash must be a function')
-    global code_set
-    code_set = set() # memoization set
-    bytecode_list = _traverse_code(func)
-    if isinstance(bytecode_list,list):
-        return hash(b''.join(bytecode_list))
-    else:
-        return hash(bytecode_list)
+    code_set = set()  # memoization set
+    bytecode_tree = _traverse_code(analyzed_function)
+    assert isinstance(bytecode_tree, list)
+    return bytecode_tree
--- pycarol/luigi_extension/pytest/test_hash_versioning.py
+++ pycarol/luigi_extension/pytest/test_hash_versioning.py
@@ -1,49 +1,107 @@
-
 from pytest import mark
-from ..hash_versioning import get_code_hash
+from ..hash_versioning import get_bytecode_tree
 
 
 def a(x):
     return x + 5
+
+
 def b(z):
     return z + 5
+
+
 def c(x):
     return x - 5
+
 
 def a_of_a(x):
     return a(x)
 
+
 def a_of_b(x):
     return b(x)
 
+
 def eternal_recursion_a(x):
     return eternal_recursion_a(x)
+
+
 def eternal_recursion_b(x):
     return eternal_recursion_b(x)
+
+
 def mutual_recursion_a(x):
     return mutual_recursion_b(x)
+
+
 def mutual_recursion_b(x):
     return mutual_recursion_a(x)
 
 
+def initial_value_a(x=3):
+    return x + 5
+
+
+def initial_value_b(x=5):
+    return x + 5
+
+
+def outer_initial_value_a():
+    return initial_value_a()
+
+
+def outer_initial_value_b():
+    return initial_value_b()
+
+
+def nested_a():
+    def _nested_a():
+        return (0)
+
+    return _nested_a()
+
+
+def nested_b():
+    def _nested_b():
+        return (100)
+
+    return _nested_b()
+
+
+def variable_args_a(*args):
+    print(args)
+
+
+def variable_args_b(*args):
+    print(args)
+
+
 equal_functions_list = [
-    (a,a),
-    (b,a),
-    (a,b),
-    (a_of_a,a_of_b),
-    (eternal_recursion_a,eternal_recursion_b),
-    (mutual_recursion_a,mutual_recursion_b),
+    (a, a),
+    (b, a),
+    (a, b),
+    (a_of_a, a_of_b),
+    (eternal_recursion_a, eternal_recursion_b),
+    (mutual_recursion_a, mutual_recursion_b),
+    (variable_args_a, variable_args_b)
 ]
 
 different_functions_list = [
-    (a,c),
-    (b,c),
+    (a, c),
+    (b, c),
+    (mutual_recursion_a, eternal_recursion_a),
+    (initial_value_a, a),
+    (initial_value_a, initial_value_b),
+    (outer_initial_value_a, outer_initial_value_b),
+    (nested_a, nested_b)
 ]
 
-@mark.parametrize("f1,f2",equal_functions_list)
-def test_equal_functions(f1,f2):
-    assert get_code_hash(f1) == get_code_hash(f2)
 
-@mark.parametrize("f1,f2",different_functions_list)
-def test_different_functions(f1,f2):
-    assert get_code_hash(f1) != get_code_hash(f2)
+@mark.parametrize("f1,f2", equal_functions_list)
+def test_equal_functions(f1, f2):
+    assert get_bytecode_tree(f1) == get_bytecode_tree(f2)
+
+
+@mark.parametrize("f1,f2", different_functions_list)
+def test_different_functions(f1, f2):
+    assert get_bytecode_tree(f1) != get_bytecode_tree(f2)
