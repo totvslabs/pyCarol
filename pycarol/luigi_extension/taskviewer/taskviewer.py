8b420194f334f2ff3d2ad8fc95212378cd86be72
rafonseca <renanfonseca@gmail.com>
2019-06-14 17:30:54 +0200

    move generif functions to utils. new wrapper function

--- pycarol/luigi_extension/taskviewer/taskviewer.py
+++ pycarol/luigi_extension/taskviewer/taskviewer.py
@@ -1,5 +1,4 @@
-from pycarol.luigi_extension.utils import build_dag
-
+from ..utils import build_dag
 def luigi_get_sons(task) -> list:
     """
     Returns a list of required tasks. This is used in build_dag
@@ -12,30 +11,15 @@ def luigi_get_sons(task) -> list:
     """
     return task.requires()
 
-
-def find_root_in_dag(dag: dict) -> list:
+def get_dag_from_task(task:list) -> dict:
     """
-    Search in a direct acyclic graph all nodes without incoming edges
+    Wrapper around generic build_dag.
     Args:
-        dag: dictionary encoding a DAG
+        task: list of proper luigi tasks
 
     Returns:
-        root_nodes: list of root nodes
-    """
+        dag: dict encoding a DAG
 
-def get_dag_node_level(dag: dict, ) -> dict:
     """
-    Returns a dict, whose keys are nodes found in dag and values are the
-    depth of the node. Root nodes have value 0.
-    Args:
-        dag: dictionary encoding a DAG
-
-    Returns:
-        levels: dict
-    """
-    root_nodes = find_root_in_dag(dag)
-    self.dag_node_level[node] = max(level, self.dag_node_level[node])
-    if node not in self.dag:
-        return
-    for n_i in self.dag[node]:
-        self.set_dag_node_level(n_i, level + 1)
+    dag = build_dag(task,luigi_get_sons)
+    return dag
