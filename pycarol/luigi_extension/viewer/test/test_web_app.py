5984dd76fa22c5f67798d78887060519545fe34a
rafonseca <renanfonseca@gmail.com>
2019-06-19 12:17:33 +0200

    move test to correct folder

---
+++ pycarol/luigi_extension/viewer/test/test_web_app.py
@@ -0,0 +1,6 @@
+from pycarol.luigi_extension.viewer.test.pipeline_example import pipeline1
+
+def test_get_plot():
+    from pycarol.luigi_extension.viewer.bokeh_plot import get_plot_from_pipeline
+    plot = get_plot_from_pipeline(pipeline1)
+    assert plot
