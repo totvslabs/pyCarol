b5eade995a560aaf9d14bceea4c2ed00860cd0f9
rafonseca <renanfonseca@gmail.com>
2019-06-19 12:03:38 +0200

    fix module imports

--- pycarol/luigi_extension/viewer/web_app.py
+++ pycarol/luigi_extension/viewer/web_app.py
@@ -1,10 +1,9 @@
 # bokeh serve --show web_app.py
 
-from pycarol.luigi_extension.taskviewer.test.pipeline_example import pipeline1
+from pycarol.luigi_extension.viewer.test.pipeline_example import pipeline1
 
 from bokeh.plotting import curdoc
 doc = curdoc()
-
-from pycarol.luigi_extension.taskviewer.bokeh_plot import get_plot_from_pipeline
+from pycarol.luigi_extension.viewer.bokeh_plot import get_plot_from_pipeline
 plot = get_plot_from_pipeline(pipeline1)
 doc.add_root(plot)
