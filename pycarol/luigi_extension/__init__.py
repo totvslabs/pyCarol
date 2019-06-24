5d296ba657e0d504a4f22694804980c598b0ba44
rafonseca <renanfonseca@gmail.com>
2019-06-24 15:18:24 +0200

    import from new pipeline module for backward compatibility

--- pycarol/luigi_extension/__init__.py
+++ pycarol/luigi_extension/__init__.py
@@ -2,31 +2,4 @@ import warnings
 warnings.warn("Using luigi_extension is deprecated. It is now called 'pipeline'", DeprecationWarning,
               stacklevel=2)
 
-from .task import(
-    Task,
-    WrapperTask,
-    inherit_list,
-    inherit_dict
-)
-
-from .targets import (
-    CDSTarget,
-    PickleTarget,
-    KerasTarget,
-    DummyTarget,
-    JsonTarget,
-    FeatherTarget,
-    PytorchTarget,
-    PickleTarget,
-    PytorchTarget,
-    KerasTarget,
-    LocalTarget,
-    DummyTarget,
-)
-from pycarol.luigi_extension.targets.deprecated_targets import PicklePyCarolTarget, PytorchPyCarolTarget, \
-    KerasPyCarolTarget, PickleLocalTarget, KerasLocalTarget, PytorchLocalTarget, JsonLocalTarget, FeatherLocalTarget
-
-
-from pycarol.luigi_extension.viewer.task_visualization import (
-    Visualization
-)
+from pycarol.pipeline import *
