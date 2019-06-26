6d3da89b18239de5601af6269417ad1a747cb130
rafonseca <renanfonseca@gmail.com>
2019-06-26 15:06:18 +0200

    improving documentation and refactor

--- pycarol/pipeline/utils/hash_versioning/hash_versioning.py
+++ pycarol/pipeline/utils/hash_versioning/hash_versioning.py
@@ -65,12 +65,25 @@ def process_op(
         called_functions,
         parent_function,
         ):
+    """
+    Process instruction updating defined_functions, code_set and
+    called_functions accordingly
+    Args:
+        context: ix, inst, instructions. bytecode itself
+        defined_functions: set of functions defined until now
+        code_set: set of functions already inspected for hash versioning purpose
+        called_functions: set of called functions until now in this bytecode
+        parent_function: function object of this bytecode
+
+    Returns:
+
+    """
     ix, inst, instructions = context
-    if inst.opname == "MAKE_FUNCTION":  # locally defined function
+    if inst.opname == "MAKE_FUNCTION":
         defined_functions.update(
             get_name_and_code_of_MAKE_FUNCTION(*context))
 
-    if inst.opname == "IMPORT_NAME":
+    if inst.opname == "IMPORT_NAME": #TODO: tests are failing for IMPORT_NAME
         defined_functions.update(
             get_name_and_object_of_IMPORT_NAME(*context))
 
@@ -85,7 +98,23 @@ def process_op(
             code_set.add(son_function)
             called_functions.add(son_function)
 
-def traverse_code(parent_function: 'function',code_set) -> list:
+def traverse_code(parent_function: 'function', code_set,
+                  ignore_not_implemented=True) -> list:
+    """
+    This method recursively traverse the bytecodes of top_function entering
+    in every function call found. At every level it returns a list of
+    bytecodes of respective functions called.
+
+    Args:
+        parent_function: function to be analyzed at this recursion level
+        code_set: set of functions already analyzed
+        ignore_not_implemented: if True, will generate bytecode_tree where it is
+        possible. Where it is not possible, will return empty list.
+
+    Returns:
+        bytecode_tree: nested lists of bytecode
+
+    """
 
     if is_builtin(parent_function):
         # dis cannot get instructions for builtins
@@ -95,12 +124,20 @@ def traverse_code(parent_function: 'function',code_set) -> list:
     called_functions = set()
     defined_functions: dict = {}
 
+    # linear search on bytecodes for called functions
     instructions = list(dis.get_instructions(parent_function))
     for context in enumerate_with_context(instructions):
-        process_op(context,defined_functions,code_set,called_functions,parent_function)
+        try:
+            process_op(context,defined_functions,code_set,called_functions,parent_function)
+        except NotImplementedError as e:
+            if ignore_not_implemented:
+                continue
+            else:
+                raise e
 
     # recursion step
-    code_list = [ traverse_code(f,code_set) for f in called_functions ]
+    code_list = [traverse_code(f, code_set, ignore_not_implemented) for f in
+                 called_functions]
 
     # leaf node step
     if is_function(parent_function):
@@ -132,12 +169,6 @@ def get_bytecode_tree(
         ignore_not_implemented=False,
 ) -> list:
     """
-    This method recursively traverse the bytecodes of top_function entering
-    in every function call found. At every level it returns a list of
-    bytecodes of respective functions called. The recursion is implemented by
-    local function traverse_code.
-
-
     Args:
         top_function: analyzed function
         ignore_not_implemented: if True, will generate bytecode_tree where it is
@@ -149,31 +180,29 @@ def get_bytecode_tree(
     """
     assert is_function(top_function), 'argument should be a function'
     code_set = set()  # "global" memoization set
-    bytecode_tree = traverse_code(top_function,code_set)
+    bytecode_tree = traverse_code(top_function, code_set,ignore_not_implemented)
 
     return bytecode_tree
 
 
-def get_function_hash(f: 'function', ignore_not_found_function=False) -> int:
+def get_function_hash(f: 'function', ignore_not_implemented=False) -> int:
     """
     Module main function. It returns a proper hash for the given function.
     Args:
         f: function to be hashed
-        ignore_not_found_function: setting to True will ignore some errors
+        ignore_not_implemented: if True, will consider bytecode_tree where it is
+        possible. Where it is not possible, will replace by an empty list.
         and compute a hash anyway. Some part of the code may be ignored.
 
     Returns:
         h: a hash number for the given function
 
     """
-    bytecode_nested_list = get_bytecode_tree(f, ignore_not_found_function)
+    bytecode_nested_list = get_bytecode_tree(f, ignore_not_implemented)
     bytecode_flat_list = flat_list(bytecode_nested_list)
     h = hash(tuple(bytecode_flat_list))
     return h
 
 
 
-
-# TODO: improve documentation
-# TODO: real scenario test case
-# TODO: support inner imports. possible?
+# TODO: production pipeline test case
