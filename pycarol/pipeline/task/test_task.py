e3a18d63e7e9ae2d41745ce39df6d5188778590e
rafonseca <renanfonseca@gmail.com>
2019-07-01 17:06:54 -0300

    update tests to use task_function

--- pycarol/pipeline/task/test_task.py
+++ pycarol/pipeline/task/test_task.py
@@ -1,5 +1,14 @@
 from .task import *
 
+def f1(x):
+    return x+1
+
+def f2(x):
+    return x+1
+
+def f3(x):
+    return f3(x+1)
+
 def test_empty_task():
     class Task1(Task):
         pass
@@ -8,41 +17,37 @@ def test_empty_task():
 
 def test_if_task_generates_hash():
     class Task1(Task):
-        pass
+        task_function = f1
     t1 = Task1()
     h = t1.hash_version()
-    print(f"hash_version={h}")
-    assert h
+    assert isinstance(h,int)
+    assert h !=0
 
 
 def test_if_tasks_has_equal_hash():
     class Task1(Task):
-        pass
+        task_function = f1
 
     class Task2(Task):
-        pass
+        task_function = f2
 
     assert Task1().hash_version() == Task2().hash_version()
 
 def test_if_tasks_has_different_hash_1():
     class Task1(Task):
-        pass
+        task_function = f1
         
-    class Task2(Task):
-        def run(self,inputs):
-            return None
+    class Task3(Task):
+        task_function = f3
 
-    assert Task1().hash_version() != Task2().hash_version()
+    assert Task1().hash_version() != Task3().hash_version()
 
 def test_if_tasks_has_different_hash_2():
     class Task1(Task):
-        def easy_run(self,inputs):
-            return inputs
+        pass
         
     class Task2(Task):
-        def easy_run(self,inputs):
-            return None
+        task_function = f1
 
     assert Task1().hash_version() != Task2().hash_version()
 
-#TODO: hash versioning is not inspecting easy_run because it is class method. Either implement class method support in hash versioning or force function reference in Task definition
