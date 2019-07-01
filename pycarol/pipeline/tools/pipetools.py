c1b273b94c658ec2ddeade9e63a7120d313338c2
rafonseca <renanfonseca@gmail.com>
2019-07-01 17:07:26 -0300

    WIP: class Pipe

--- pycarol/pipeline/tools/pipetools.py
+++ pycarol/pipeline/tools/pipetools.py
@@ -27,8 +27,12 @@ def get_dag_from_task(task:list) -> dict:
     dag = build_dag(task,luigi_get_sons)
     return dag
 
-
+#TODO: implement the following methods
 class Pipe(object):
+    """
+    This class should be used to compose a pipeline given a list of tasks. It
+    contains many methods to interact with the pipeline as a whole.
+    """
     def __init__(self, tasks: list):
         assert isinstance(tasks,list)
 
@@ -37,13 +41,43 @@ class Pipe(object):
         self.top_nodes = tasks
         self.dag = get_dag_from_task(tasks)
         self.all_tasks = [k for k in self.dag]
-    def remove_upstream(self, task):
-        return
+
     def remove_all(self):
+        """Remove all targets related to this pipeline."""
+        for t in self.all_tasks:
+            t.remove()
+
+    def remove_upstream(self, task):
+        """Remove all targets in this pipeline that depend on the given task."""
         return
-    def describe_targets(self):
+
+    def remove_obsolete(self):
+        """Remove all targets whose hash_versions do not match to current version"""
         return
-    def get_task_by_name(self,name):
+    
+    def remove_orphans(self):
+        """Remove all targets for which respective downstream targets are not complete"""
         return
-    def get_dag(self):
-        return self.dag
+
+    def get_task_by_name(self,name:str)->list:
+        """Given the name of a task returns a list of matching task objects"""
+        return
+
+    def run(self):
+        """Run the whole pipeline"""
+        return
+    
+    def run_partial(self,task):
+        """Run the pipeline until given task is achieved"""
+        return
+
+    def run_task(self,task):
+        """Run single task if requirements are satisfied"""
+        return
+
+    # def get_complete(self):
+    #     return
+
+    # def describe_targets(self)-> str:
+    #     """Returns a text summary informing the status of all related targets."""
+    #     return
