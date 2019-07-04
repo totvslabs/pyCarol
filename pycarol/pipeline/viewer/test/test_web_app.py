445fbe08c35de78c074738db9a94077e02624b91
rafonseca <renanfonseca@gmail.com>
2019-07-04 17:22:59 -0300

    add bokeh doc test

--- pycarol/pipeline/viewer/test/test_web_app.py
+++ pycarol/pipeline/viewer/test/test_web_app.py
@@ -1,5 +1,5 @@
 from pycarol.pipeline.tools.pipeline_example import pipeline1
-import warnings
+
 def test_get_plot():
     from pycarol.pipeline.viewer.bokeh_plot import get_plot_from_pipeline
     plot = get_plot_from_pipeline(pipeline1)
