2349ee47d0a3c8935f5600269f1a39bb57b1b4a4
rafonseca <renanfonseca@gmail.com>
2019-07-06 11:05:14 -0300

    new test

--- pycarol/pipeline/viewer/test/test_web_app.py
+++ pycarol/pipeline/viewer/test/test_web_app.py
@@ -10,8 +10,30 @@ def test_bokeh_doc():
     from pycarol.pipeline.tools.pipeline_example import pipeline1
     from pycarol.pipeline.viewer.bokeh_plot import get_plot_from_pipeline
     plot = get_plot_from_pipeline(pipeline1)
-    from bokeh.plotting import curdoc
-    
+    from bokeh.plotting import curdoc    
     doc = curdoc()
-    doc.add_root(plot)
 
+def test_start_app():
+    #WIP
+    from tornado.ioloop import IOLoop
+
+    from bokeh.application.handlers import FunctionHandler
+    from bokeh.application import Application
+    from bokeh.server.server import Server
+
+    from pycarol.pipeline.tools.pipeline_example import pipeline1
+    from pycarol.pipeline.viewer.bokeh_plot import get_plot_from_pipeline
+    plot = get_plot_from_pipeline(pipeline1)
+    modify_doc = lambda doc: doc.add_root(plot)
+
+    io_loop = IOLoop.current()
+    bokeh_app = Application(FunctionHandler(modify_doc))
+
+    server = Server({'/': bokeh_app}, io_loop=io_loop)
+
+    server.start()
+
+    io_loop.add_callback(server.show, "/")
+    io_loop.add_callback(io_loop.stop)
+    io_loop.start()
+    io_loop.close()
