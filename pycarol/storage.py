7f5a926c747200cdf2e5e764f4c0032f7b31b0b7
rafonseca <renanfonseca@gmail.com>
2019-07-17 11:28:31 -0300

    use class variable in Storage.backend

--- pycarol/storage.py
+++ pycarol/storage.py
@@ -2,29 +2,29 @@ from .carolina import Carolina
 
 
 class Storage:
+    backend = None
     def __init__(self, carol):
         self.carol = carol
-        self.backend = None
         self._init_if_needed()
 
     def _init_if_needed(self):
-        if self.backend is not None:
+        if Storage.backend is not None:
             return
 
         carolina = Carolina(self.carol)
         carolina.init_if_needed()
         if carolina.engine == 'GCP-CS':
             from .utils.storage_gcpcs import StorageGCPCS
-            self.backend = StorageGCPCS(self.carol, carolina)
+            Storage.backend = StorageGCPCS(self.carol, carolina)
         elif carolina.engine == 'AWS-S3':
             from .utils.storage_awss3 import StorageAWSS3
-            self.backend = StorageAWSS3(self.carol, carolina)
+            Storage.backend = StorageAWSS3(self.carol, carolina)
 
     def save(self, name, obj, format='pickle', parquet=False, cache=True):
-        self.backend.save(name, obj, format, parquet, cache)
+        Storage.backend.save(name, obj, format, parquet, cache)
 
     def load(self, name, format='pickle', parquet=False, cache=True, storage_space='app_storage', columns=None):
-        return self.backend.load(name=name, format=format, parquet=parquet, cache=cache, storage_space=storage_space,
+        return Storage.backend.load(name=name, format=format, parquet=parquet, cache=cache, storage_space=storage_space,
                                  columns=columns)
 
     def files_storage_list(self, app_name=None, all_apps=False, print_paths=False):
@@ -42,34 +42,34 @@ class Storage:
         :return: list of files paths.
         """
 
-        return self.backend.files_storage_list(app_name=app_name, all_apps=all_apps, print_paths=print_paths)
+        return Storage.backend.files_storage_list(app_name=app_name, all_apps=all_apps, print_paths=print_paths)
 
     def exists(self, name):
-        return self.backend.exists(name)
+        return Storage.backend.exists(name)
 
     def delete(self, name):
-        self.backend.delete(name)
+        Storage.backend.delete(name)
 
     def build_url_parquet_golden(self, dm_name):
-        return self.backend.build_url_parquet_golden(dm_name)
+        return Storage.backend.build_url_parquet_golden(dm_name)
 
     def build_url_parquet_staging(self, staging_name, connector_id):
-        return self.backend.build_url_parquet_staging(staging_name, connector_id)
+        return Storage.backend.build_url_parquet_staging(staging_name, connector_id)
 
     def build_url_parquet_staging_master(self, staging_name, connector_id):
-        return self.backend.build_url_parquet_staging_master(staging_name, connector_id)
+        return Storage.backend.build_url_parquet_staging_master(staging_name, connector_id)
 
     def build_url_parquet_staging_rejected(self, staging_name, connector_id):
-        return self.backend.build_url_parquet_staging_rejected(staging_name, connector_id)
+        return Storage.backend.build_url_parquet_staging_rejected(staging_name, connector_id)
 
     def get_dask_options(self):
-        return self.backend.get_dask_options()
+        return Storage.backend.get_dask_options()
 
     def get_golden_file_paths(self, dm_name):
-        return self.backend.get_golden_file_paths(dm_name)
+        return Storage.backend.get_golden_file_paths(dm_name)
 
     def get_view_file_paths(self, view_name):
-        return self.backend.get_view_file_paths(view_name)
+        return Storage.backend.get_view_file_paths(view_name)
 
     def get_staging_file_paths(self, staging_name, connector_id):
-        return self.backend.get_staging_file_paths(staging_name, connector_id)
+        return Storage.backend.get_staging_file_paths(staging_name, connector_id)
