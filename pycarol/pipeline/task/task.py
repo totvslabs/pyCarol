a0c75913e8e34f679821b34aa78829bc39f088d0
rafonseca <renanfonseca@gmail.com>
2019-07-19 20:36:24 -0300

    alternative way to define pipeline

--- pycarol/pipeline/task/task.py
+++ pycarol/pipeline/task/task.py
@@ -21,7 +21,11 @@ class Task(luigi.Task):
     task_function = None
     version = '0.0.0'
     metadata = {}
-    
+
+    def __call__(self,*args):
+        """Alternative way to define pipeline"""
+        return inherit_list(*args)(self.__class__)
+
     def buildme(self, local_scheduler=True, **kwargs):
         luigi.build([self, ], local_scheduler=local_scheduler, **kwargs)
 
