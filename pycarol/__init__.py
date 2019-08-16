bbc7e71af28c1b46ca2863d0680d7174573ddb9c
Rafael <rafarui@gmail.com>
2019-08-16 20:32:54 -0300

    remove cals metadata

--- pycarol/__init__.py
+++ pycarol/__init__.py
@@ -11,7 +11,8 @@ __TEMP_STORAGE__ = os.path.join(tempfile.gettempdir(), 'carolina/cache')
 
 __CONNECTOR_PYCAROL__ = 'f9953f6645f449baaccd16ab462f9b64'
 
-_CAROL_METADATA = ['mdmCounterForEntity', 'mdmId', 'mdmLastUpdated', 'mdmTenantId', 'mdmConnectorId', 'mdmEntityType']
+_CAROL_METADATA = ['mdmCounterForEntity', 'mdmId', 'mdmLastUpdated',
+                   'mdmConnectorId']
 
 
 from .carol import Carol
