e583340409e5d91526a6e075d7ddf32f688f7841
Alexandre Guimarães Malucelli <alexandre@malucelli.net>
2020-01-22 20:33:47 -0300

    fix: unzip quiet

--- hack/setup.sh
+++ hack/setup.sh
@@ -33,7 +33,7 @@ if ! command -v sonar-scanner >/dev/null 2>&1; then
 	}
 	curl -fL "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.2.0.1873-${OSNAME}.zip" \
 		-o /tmp/sonar-scanner.zip
-	unzip /tmp/sonar-scanner.zip -d /tmp
+	unzip -q /tmp/sonar-scanner.zip -d /tmp
 	mv /tmp/sonar-scanner-* ./bin/sonar-scanner
 fi
 
