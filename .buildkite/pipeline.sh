0f3e7a2b9621250296108e8b244748ba63af25b1
Alexandre Guimarães Malucelli <alexandre@malucelli.net>
2020-01-22 16:32:05 -0300

    fix: ensure a single node is used

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,35 +1,19 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Outputs a pipeline that targets agents that have the same 'name' meta-data
-# value as the step that does the pipeline upload. This means that all the
-# steps will run on the same agent machine, assuming that the 'name' meta-data
-# value is unique to each agent.
-#
-# Each agent needs to be configured with meta-data like so:
-#
-# meta-data="name=<unique-name>"
-#
-# To use, save this file as .buildkite/pipeline.sh, chmod +x, and then set your
-# first pipeline step to run this and pipe it into pipeline upload:
-#
-# .buildkite/pipeline.sh | buildkite-agent pipeline upload
-#
-
-agent_name=$(buildkite-agent meta-data get name)
-
-cat << EOF
+cat <<EOF
 steps:
     - label: ":buildkite: Build Image"
       command: make docker_ci
       agents:
-        name: "${agent_name}"
+        name: "${BUILDKITE_AGENT_NAME}"
 
     - wait: ~
 
     - label: ":hammer: Tests"
       command: docker run --rm -it -e SONAR_PYCAROL_TOKEN=${SONAR_PYCAROL_TOKEN} -e BUILDKITE_BRANCH=${BUILDKITE_BRANCH} pycarolci make code_scan
       agents:
-        name: "${agent_name}"
+        name: "${BUILDKITE_AGENT_NAME}"
 
     - wait: ~
 
@@ -37,7 +21,7 @@ steps:
       command: docker run --rm -it pycarolci make deploy
       branches: "*.*.*"
       agents:
-        name: "${agent_name}"
+        name: "${BUILDKITE_AGENT_NAME}"
 
     - wait: ~
 
@@ -45,13 +29,12 @@ steps:
       command: make docker
       branches: "*.*.*"
       agents:
-        name: "${agent_name}"
+        name: "${BUILDKITE_AGENT_NAME}"
 
     - wait: ~
 
     - label: ":recycle: Clean up"
       command: docker rmi pycarolci
       agents:
-        name: "${agent_name}"
-
+        name: "${BUILDKITE_AGENT_NAME}"
 EOF
