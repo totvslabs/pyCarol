0924a9f489352de56b98791709c767bfe4b2943f
Alexandre Guimarães Malucelli <alexandre@malucelli.net>
2020-01-22 19:30:22 -0300

    fix: remove custom buildkite agent

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -3,38 +3,16 @@ set -e
 
 cat <<EOF
 steps:
-    - label: ":buildkite: Build Image"
-      command: make docker_ci
-      agents:
-        name: "${BUILDKITE_AGENT_NAME}"
-
-    - wait: ~
-
-    - label: ":hammer: Tests"
-      command: docker run --rm -it -e SONAR_PYCAROL_TOKEN=${SONAR_PYCAROL_TOKEN} -e BUILDKITE_BRANCH=${BUILDKITE_BRANCH} pycarolci make code_scan
-      agents:
-        name: "${BUILDKITE_AGENT_NAME}"
-
-    - wait: ~
-
-    - label: ":shipit: Deploy"
-      command: docker run --rm -it pycarolci make deploy
-      branches: "*.*.*"
-      agents:
-        name: "${BUILDKITE_AGENT_NAME}"
-
-    - wait: ~
-
-    - label: ":docker: pyCarol Image"
-      command: make docker
-      branches: "*.*.*"
-      agents:
-        name: "${BUILDKITE_AGENT_NAME}"
-
-    - wait: ~
-
-    - label: ":recycle: Clean up"
-      command: docker rmi pycarolci
-      agents:
-        name: "${BUILDKITE_AGENT_NAME}"
+  - label: ":python: Build"
+    command: make ci
+    agents:
+      queue: "default"
+    timeout_in_minutes: 10
+  - wait: ~
+  - label: ":python: Release"
+    command: make release
+    branches: "*.*.*"
+    agents:
+      queue: "default"
+    timeout_in_minutes: 10
 EOF
