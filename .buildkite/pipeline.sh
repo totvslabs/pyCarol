a22f1d478341e99bc851c956de3d7d1a90301d8a
Alexandre Malucelli <alexandre@malucelli.net>
2020-01-23 12:35:27 -0300

    fix: pycarol release pipeline (#425)
    * fix: renamed and added pycarol version arg
    
    * feat: relase docker only on tags
    
    * fix: ensure a single node is used
    
    * feat: added editorconfig
    
    * feat: added local bin folder for temp binaries
    
    * fix: docker env vars
    
    * fix: remove custom buildkite agent
    
    * fix: added deploy into a custom script
    
    * feat: added a setup script
    
    * fix: improved makefile
    
    * fix: improved pypi
    
    * fix: missing pkgs
    
    * fix: install order
    
    * fix: cmd should be evaluated during the runtime
    
    * fix: cmds
    
    * fix: unzip quiet
    
    * fix: quiet mode
    
    * fix: quiet sonar
    
    * fix: messages on build
    
    * fix: improved msgs
    
    * chore: better desc
    
    * fix: improved pypi file
    
    * feat: added wait for package on pypi
    
    * feat: added debug opt
    
    * fix: improved sonar logic
    
    * fix: braces for single cmds

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,49 +1,18 @@
-#!/bin/bash
+#!/bin/sh
+set -e
 
-# Outputs a pipeline that targets agents that have the same 'name' meta-data
-# value as the step that does the pipeline upload. This means that all the
-# steps will run on the same agent machine, assuming that the 'name' meta-data
-# value is unique to each agent.
-#
-# Each agent needs to be configured with meta-data like so:
-#
-# meta-data="name=<unique-name>"
-#
-# To use, save this file as .buildkite/pipeline.sh, chmod +x, and then set your
-# first pipeline step to run this and pipe it into pipeline upload:
-#
-# .buildkite/pipeline.sh | buildkite-agent pipeline upload
-#
-
-agent_name=$(buildkite-agent meta-data get name)
-
-cat << EOF
+cat <<EOF
 steps:
-    - label: ":docker: Build"
-      command: docker build --build-arg pypi_user=${PYPI_USERNAME} --build-arg pypi_pass=${PYPI_PASSWORD} -f Dockerfile.buildkite -t pycarolci .
-      agents:
-        name: "${agent_name}"
-
-    - wait: ~
-
-    - label: ":hammer: Tests"
-      command: docker run --rm -it -e SONAR_PYCAROL_TOKEN=${SONAR_PYCAROL_TOKEN} -e BUILDKITE_BRANCH=${BUILDKITE_BRANCH} pycarolci make code_scan
-      agents:
-        name: "${agent_name}"
-
-    - wait: ~
-
-    - label: ":shipit: Deploy"
-      command: docker run --rm -it pycarolci make deploy
-      branches: "master"
-      agents:
-        name: "${agent_name}"
-
-    - wait: ~
-
-    - label: ":recycle: Clean up"
-      command: docker rmi pycarolci
-      agents:
-        name: "${agent_name}"
-
+  - label: ":python: Configuring, Packaging, Testing and Scanning"
+    command: make ci
+    agents:
+      queue: "default"
+    timeout_in_minutes: 10
+  - wait: ~
+  - label: ":python: Releasing the Package and Image"
+    command: make release
+    branches: "*.*.*"
+    agents:
+      queue: "default"
+    timeout_in_minutes: 15
 EOF
