e9d03d967df2ab6381017de042d46835e8ef0a55
Rafael Rui <rafarui@users.noreply.github.com>
2020-02-03 10:34:13 -0300

    2.29.0 (#430)
    * Bump version: 2.28.1 → 2.29.0
    
    * Pipe is changing external dictionaries. (#428)
    
    * 2.29 retry checksum (#429)
    
    * Add retry when loading from cds.
    
    * add max retry
    
    * add max retry
    
    * typo

---
+++ pycarol/utils/_cds_utils.py
@@ -0,0 +1,29 @@
+from google.resumable_media import DataCorruption
+import functools
+
+
+def retry_check_sum(func):
+    """
+    This is a decorator to retry when getting the
+    `Checksum mismatch while downloading:` error.
+
+    Args:
+        func: pycarol.Storage.load
+
+    Returns:
+
+    """
+
+    @functools.wraps(func)
+    def retry(*args, **kwargs):
+
+        for i in range(5):
+            try:
+                return func(*args, **kwargs)
+            except DataCorruption as e:
+                # TODO: Add logs in pycarol.
+                continue
+
+        raise Exception(f"Max retries exceeded with {func}")
+
+    return retry
