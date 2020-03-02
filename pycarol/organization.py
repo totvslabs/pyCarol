adc210bf5f8e42964a0a824ab0521895fb40d07e
Rafael <rafarui@gmail.com>
2020-03-01 19:59:03 -0500

    switch context

---
+++ pycarol/organization.py
@@ -0,0 +1,37 @@
+
+
+class Organization:
+
+    def __init__(self, carol):
+        self.carol = carol
+
+    def get_organization_info(self, subdomain):
+        """
+        Get organization information.
+
+        :param subdomain: `str`
+            Organization subdomain
+        :return:
+            dict with the information about the organization.
+        """
+
+        return self.carol.call_api(f'v1/organizations/domain/{subdomain}',
+                                   auth=False, status_forcelist=[], retries=0)
+
+
+    def get_environment_info(self, environment):
+        """
+        Get environment information.
+
+        :param environment: `str`
+            Tenant name
+        :return:
+            dict with the information about the environment.
+        """
+        return self.carol.call_api(f'v2/tenants/domain/{environment}',
+                                   auth=False, status_forcelist=[], retries=0)
+
+
+
+
+
