2f5a7a8eabf00888c8b5ee7ce1cfbdbf2b951311
rafarui <rafarui@gmail.com>
2020-04-24 20:43:17 -0400

    add support to lazy execute when using the PwdKeyAuth

--- pycarol/auth/PwdKeyAuth.py
+++ pycarol/auth/PwdKeyAuth.py
@@ -9,12 +9,18 @@ class PwdKeyAuth:
     Args:
         access_token: `str`
             Carol access token created using the user and password.
+        lazy_login: `bool` default `False`
+            It will not validate the `access_token` when starting the instance.
+            This is used to speedup requests, when the user knows that this token is valid. Using this option, one lose
+            all refresh token capability.
     """
-    def __init__(self, access_token):
+
+    def __init__(self, access_token, lazy_login=False):
         self.access_token = access_token
         self._token = None
         self.carol = None
         self.connector_id = None
+        self.lazy_login = lazy_login
 
     def set_connector_id(self, connector_id):
         self.connector_id = connector_id
@@ -31,6 +37,14 @@ class PwdKeyAuth:
 
         """
         self.carol = carol
+
+        if self.lazy_login:
+            self._token = types.SimpleNamespace()
+            self._token.access_token = self.access_token
+            self._token.refresh_token = None
+            self._token.expiration = float('inf')
+            return
+
 
         resp = self.carol.call_api(f'v2/oauth2/token/{self.access_token}', method='GET', auth=False,
                                    content_type='application/x-www-form-urlencoded')
