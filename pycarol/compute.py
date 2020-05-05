3e9754ff97e615aac5e7cf948b786802e68e284f
rafarui <rafarui@gmail.com>
2020-05-04 21:32:10 -0400

    logs

--- pycarol/compute.py
+++ pycarol/compute.py
@@ -1,7 +1,4 @@
-
-
 class Compute:
-
     """
     Compute.
 
@@ -11,8 +8,43 @@ class Compute:
         self.carol = carol
 
     def get_machine_types(self):
-
         response = self.carol.call_api(path=f'v1/compute/machineTypes',
                                        method='GET')
+
+        return response
+
+    def get_app_logs(self, app_name=None, app_type='carolApp', severity='INFO', filters=None, page_size=10, ):
+        """
+        Get execution logs for an app.
+
+        Args:
+            app_name: `str` default `None`
+                Carol app name. It will overwrite the app name used in Carol() initialization.
+            app_type: `str` default `carolApp`
+                Possible values `carolApp` or `carolConnect`
+            severity: `str` default `INFO`
+                Minimal log to fetch.
+            filters: `dict` default `None`
+                Extra filters.
+            page_size: `int` default 10
+                Number of records to return.
+        Returns:
+
+        """
+
+        if app_name is None:
+            app_name = self.carol.app_name
+
+        filters = filters if filters else ""
+
+        params = {
+            "appType": app_type,
+            "appName": app_name,
+            "pageSize": page_size,
+            "severity": severity
+        }
+
+        response = self.carol.call_api(path=f'v1/log/retrieveLogs', data=filters,
+                                       method='POST', params=params, content_type='application/octet-stream')
 
         return response
