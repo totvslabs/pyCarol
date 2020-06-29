407c5b61f8d900c76054dc7d721f2dbeb3e87753
Rafael Rui <rafarui@users.noreply.github.com>
2020-06-29 15:36:46 -0400

    orf mdmId fix (#480)

--- pycarol/organization.py
+++ pycarol/organization.py
@@ -9,31 +9,39 @@ class Organization:
     """
 
     def __init__(self, carol):
-
         self.carol = carol
 
-    def get_organization_info(self, organization):
+    def get_organization_info(self, organization, auth=True):
         """
-        Get organization information.
 
-        :param organization: `str`
-            Organization subdomain
-        :return:
-            dict with the information about the organization.
+        Args:
+            organization: `str`
+                Organization subdomain
+            auth: `bool`
+            If send the auth information. Some fields do not come if not logged.
+
+        Returns:
+             dict with the information about the organization.
+
         """
 
         return self.carol.call_api(f'v1/organizations/domain/{organization}',
-                                   auth=False, status_forcelist=[], retries=0)
+                                   auth=auth, status_forcelist=[], retries=0)
 
-
-    def get_environment_info(self, environment):
+    def get_environment_info(self, environment, auth=True):
         """
         Get environment information.
 
-        :param environment: `str`
-            Tenant name
-        :return:
+        Args:
+            environment: `str`
+                Tenant name
+            auth: `bool`
+                If send the auth information. Some fields do not come if not logged.
+
+        Returns:
             dict with the information about the environment.
+
         """
+
         return self.carol.call_api(f'v2/tenants/domain/{environment}',
-                                   auth=False, status_forcelist=[], retries=0)
+                                   auth=auth, status_forcelist=[], retries=0)
