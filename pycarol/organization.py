989c00ade2c7e3d2ead6d4018896aae934b53658
Rafael Rui <rafarui@users.noreply.github.com>
2020-11-24 07:53:46 -0500

    bug: do not use auth in get_by_id (#512)
    * auth option
    
    * reformat file

--- pycarol/organization.py
+++ pycarol/organization.py
@@ -46,35 +46,36 @@ class Organization:
         return self.carol.call_api(f'v2/tenants/domain/{environment}',
                                    auth=auth, status_forcelist=[], retries=0)
 
-    def get_org_by_id(self, org_id):
-
+    def get_org_by_id(self, org_id, auth=True):
         """
         Get organization info by id.
 
         Args:
             org_id: `str`
                 Organization Id
+            auth: `bool`
+                If send the auth information. Some fields do not come if not logged.
 
         Returns:
             dict with the information about the organization.
 
         """
 
-        return self.carol.call_api(f'v1/organizations/{org_id}', method='GET')
+        return self.carol.call_api(f'v1/organizations/{org_id}', method='GET', auth=auth)
 
-
-    def get_env_by_id(self, env_id):
-
+    def get_env_by_id(self, env_id, auth=True):
         """
         Get env. info by id.
 
         Args:
             env_id: `str`
                 Organization Id
+            auth: `bool`
+                If send the auth information. Some fields do not come if not logged.
 
         Returns:
             dict with the information about the organization.
 
         """
 
-        return self.carol.call_api(f'v2/admin/tenants/{env_id}', method='GET')
+        return self.carol.call_api(f'v2/admin/tenants/{env_id}', method='GET', auth=auth)
