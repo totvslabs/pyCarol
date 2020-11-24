0db13ca515ccaaa0f0c044ed953fe6a196bce204
rafarui <rafarui@gmail.com>
2020-11-24 09:47:15 -0300

    auth option

--- pycarol/organization.py
+++ pycarol/organization.py
@@ -46,7 +46,7 @@ class Organization:
         return self.carol.call_api(f'v2/tenants/domain/{environment}',
                                    auth=auth, status_forcelist=[], retries=0)
 
-    def get_org_by_id(self, org_id):
+    def get_org_by_id(self, org_id, auth=True):
 
         """
         Get organization info by id.
@@ -54,16 +54,18 @@ class Organization:
         Args:
             org_id: `str`
                 Organization Id
+            auth: `bool`
+                If send the auth information. Some fields do not come if not logged.
 
         Returns:
             dict with the information about the organization.
 
         """
 
-        return self.carol.call_api(f'v1/organizations/{org_id}', method='GET')
+        return self.carol.call_api(f'v1/organizations/{org_id}', method='GET', auth=auth)
 
 
-    def get_env_by_id(self, env_id):
+    def get_env_by_id(self, env_id, auth=True):
 
         """
         Get env. info by id.
@@ -71,10 +73,12 @@ class Organization:
         Args:
             env_id: `str`
                 Organization Id
+            auth: `bool`
+                If send the auth information. Some fields do not come if not logged.
 
         Returns:
             dict with the information about the organization.
 
         """
 
-        return self.carol.call_api(f'v2/admin/tenants/{env_id}', method='GET')
+        return self.carol.call_api(f'v2/admin/tenants/{env_id}', method='GET', auth=auth)
