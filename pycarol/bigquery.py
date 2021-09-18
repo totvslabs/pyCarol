d864d17ce112d9327c18a240822c6cc241e07826
darolt <jeandarolt@gmail.com>
2021-09-17 21:48:44 -0300

    adding bigquery frontend + minor refactor

--- pycarol/bigquery.py
+++ pycarol/bigquery.py
@@ -5,6 +5,9 @@ import pandas as pd
 from google.cloud import bigquery
 from google.oauth2.service_account import Credentials
 
+from .carol import Carol
+from .connectors import Connectors
+
 
 def get_service_account() -> T.Dict[str, str]:
     """Get BigQuery credentials from Carol."""
@@ -22,22 +25,35 @@ def query(client: bigquery.Client, query_: str) -> pd.DataFrame:
     return client.query(query_).to_dataframe()
 
 
-def get_template_vars(query_: str) -> T.Set[T.Tuple[str, str]]:
-    regex = re.compile(
-        "{{\s*((?P<connector_name>[0-9A-z]+)(\.))?(?P<table_name>[0-9A-z]+.)\s*}}"
-    )
-    return {
-        (match.group("connector_name"), match.group("table_name"))
-        for match in regex.finditer(query_)
+def prepare_query(
+    carol: Carol,
+    query_: str,
+) -> str:
+    """Render template replacing variables (if any) with values.
+
+    {{connector_name.staging_table}} is replaced by:
+        `TENANTID.stg_CONNECTORID_STAGINGNAME`
+    {{datamodel_name}} is replaced by `TENANTID.dm_MODELNAME`
+    Variables must follow the '{{variable}}' pattern.
+
+    Args:
+        carol: Carol object.
+        query_: BigQuery SQL query.
+
+    Return:
+        Query string with template rendered.
+    """
+    template_vars = _get_template_vars(query_)
+    if len(template_vars) == 0:
+        return query_
+
+    connectors = Connectors(carol)
+    tenant_id = carol.tenant["mdmId"]
+    connector_names = {name_ for name_, _ in template_vars if name_ is not None}
+    connector_map = {
+        name_: connectors.get_by_name(name_)["mdmId"] for name_ in connector_names
     }
 
-
-def prepare_query(
-    query_: str,
-    tenant_id: str,
-    template_vars: T.Set[T.Tuple[str, str]],
-    connector_map: T.Dict[str, str],
-) -> str:
     replace_map = {}
     for connector_name, table_name in template_vars:
         if connector_name is not None:
@@ -55,3 +71,24 @@ def prepare_query(
         raise ValueError()
 
     return re.sub(r"({{\s*([0-9A-z\.]+)\s*}})", _replace_func, query_)
+
+
+def _get_template_vars(query_: str) -> T.Set[T.Tuple[str, str]]:
+    """Get all variables in the template.
+
+    Variables follow the '{{connector_name.table_name}}' pattern. Connector name is
+    optional.
+
+    Args:
+        query_: BigQuery SQL query.
+
+    Return:
+        Set with connector name (None when there is none) and staging/model name.
+    """
+    regex = re.compile(
+        "{{\s*((?P<connector_name>[0-9A-z]+)(\.))?(?P<table_name>[0-9A-z]+.)\s*}}"
+    )
+    return {
+        (match.group("connector_name"), match.group("table_name"))
+        for match in regex.finditer(query_)
+    }
--- pycarol/sql.py
+++ pycarol/sql.py
@@ -1,36 +1,44 @@
-import warnings
-from pycarol import PwdAuth, PwdKeyAuth
 from contextlib import contextmanager
-import json
 import itertools
+import json
+import typing as T
+import warnings
+
+import pandas as pd
+
+from pycarol import Carol, PwdAuth, PwdKeyAuth
+from . import bigquery
 
 
 @contextmanager
 def SQLSocket(carol, *args, **kw):
-    """
-    Create Socket connection to Carol.
+    """Create Socket connection to Carol.
 
     Args:
         carol (Carol): Carol instance.
-    
     """
     try:
         from websocket import create_connection
     except ModuleNotFoundError:
-        warnings.warn("""
+        warnings.warn(
+            """
             websocket-client is not installed.
             Please install it with:
                 pip install websocket-client
-        """, Warning)
+        """,
+            Warning,
+        )
         raise ModuleNotFoundError
-    
+
     if not isinstance(carol.auth, (PwdAuth, PwdKeyAuth)):
         raise TypeError("Websocket only works with `PwdAuth`, `PwdKeyAuth`")
-    def build_ws_url():
-        return f'wss://{carol.organization}.carol.ai/websocket/live/{carol.auth._token.access_token}'
-    
-    ws = create_connection(build_ws_url())
-    user_id = carol.call_api('v1/users/current')['mdmId']
+
+    def _build_ws_url():
+        token = carol.auth._token.access_token
+        return f"wss://{carol.organization}.carol.ai/websocket/live/{token}"
+
+    ws = create_connection(_build_ws_url())
+    user_id = carol.call_api("v1/users/current")["mdmId"]
     subs = {"action": "SUBSCRIBE", "type": "SQL_RESPONSE", "subtype": user_id}
     ws.send(json.dumps(subs))
     print(json.loads(ws.recv()))
@@ -40,22 +48,24 @@ def SQLSocket(carol, *args, **kw):
         ws.close()
 
 
-
 class SQL:
     def __init__(self, carol):
         self.carol = carol
-        warnings.warn("""
-        Experimental feature. The API might change without notice.
-        """)
+        warnings.warn("Experimental feature. The API might change without notice.")
 
-            
-    def query(self, query, params=None, method='sync', dataframe=True,  **kw):
-        
-        """
-        Execute SQL Query
+    def query(
+        self,
+        query,
+        params=None,
+        method="sync",
+        dataframe=True,
+        service_account=None,
+        **kw,
+    ):
+        """Execute SQL Query.
 
         Args:
-            query: `str` 
+            query: `str`
                 SQL Query
             params: `list` default `None`
                 Params to send to Carol
@@ -64,38 +74,75 @@ class SQL:
                 if socket runs the query using web socket and returns the result
             dataframe: `bool` default `True`
                 if True returns a pandas dataframe
+            service_account: in case you have a service account for accessing BigQuery.
             **kw: `dict`
         Returns:
-        
+
         """
-        if method not in ('sync', 'socket'):
-            raise ValueError("kind must be either 'sync' or 'socket'")
-        
-        params = params or []
-        payload = {"preparedStatement": query, "params": params} 
-        
-        if method == 'socket':
-            last_result = False
-            results = []
-            with SQLSocket(self.carol, **kw) as ws:
-                resp = self.carol.call_api(path='v2/sql/query', method='POST', data=payload)
-                while not last_result:
-                    resp = ws.recv()
-                    resp = json.loads(resp)
-                    result = json.loads(resp['message'])
-                    last_result = result['lastResult']
-                    if result['success']:
-                        results.extend(result['results'])
-                    else:
-                        raise ValueError(f"{result['error']}")
-            
-        elif method == 'sync':
-            results = self.carol.call_api(path='v2/sql/querySync', method='POST', data=payload)
-            if not results[0]['success']:
-                raise ValueError(f"{results[0]['error']}")
-            results = list(itertools.chain(*[i['results'] for i in results]))
-        if dataframe:
-            import pandas as pd
-            return pd.DataFrame(results)
+        methods = ("sync", "socket", "bigquery")
+
+        if method == "socket":
+            params = params or []
+            payload = {"preparedStatement": query, "params": params}
+            results = _socket_query(self.carol, payload, **kw)
+        elif method == "sync":
+            params = params or []
+            payload = {"preparedStatement": query, "params": params}
+            results = _sync_query(self.carol, payload)
+        elif method == "bigquery":
+            return _bigquery_query(
+                self.carol, query, service_account=service_account
+            )
         else:
-            return results
+            raise ValueError(f"'method' must be either: {methods}")
+
+        if dataframe:
+            return pd.DataFrame(results)
+
+        return results
+
+
+def _socket_query(carol: Carol, payload, **kwargs):
+    last_result = False
+    results = []
+    with SQLSocket(carol, **kwargs) as ws:
+        resp = carol.call_api(path="v2/sql/query", method="POST", data=payload)
+        while not last_result:
+            resp = ws.recv()
+            resp = json.loads(resp)
+            result = json.loads(resp["message"])
+            last_result = result["lastResult"]
+            if result["success"]:
+                results.extend(result["results"])
+            else:
+                raise ValueError(f"{result['error']}")
+    return results
+
+
+def _sync_query(carol: Carol, payload):
+    results = carol.call_api(path="v2/sql/querySync", method="POST", data=payload)
+    if not results[0]["success"]:
+        raise ValueError(f"{results[0]['error']}")
+    return list(itertools.chain(*[i["results"] for i in results]))
+
+
+def _bigquery_query(
+    carol: Carol,
+    query: str,
+    service_account: T.Optional[T.Dict[str, str]] = None,
+) -> pd.DataFrame:
+    """Run query for datamodel.
+
+    Args:
+        query: BigQuery SQL query.
+        service_account: in case you have a service account for accessing BigQuery.
+
+    Returns:
+        Query result.
+    """
+    if service_account is None:  # must call carol to get service account
+        raise NotImplementedError("You must pass a service_account. Not implemented.")
+
+    query = bigquery.prepare_query(carol, query)
+    client = bigquery.generate_client(service_account)
+    return bigquery.query(client, query)
