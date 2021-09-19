eb436f847ddf2be7e2b97d313c244549a5440b42
darolt <jeandarolt@gmail.com>
2021-09-19 19:38:09 -0300

    Bigquery (#549)
    * adding script to bigquery
    
    * Bump version: 2.46.2 → 2.46.3
    
    * adding bigquery FE
    
    * adding SQL to pycarol package
    
    * removing tmp file
    
    * promoting dataframe to min
    
    * adding bigquery frontend + minor refactor
    
    * adding bigquery tests
    
    * renaming test file for bq
    
    * making bq backend independent from pandas
    
    * moving all bq stuff to bq backend
    
    * resetting default min to not have dataframe dependencies
    
    * bigquery sql returns dict

--- pycarol/__init__.py
+++ pycarol/__init__.py
@@ -5,7 +5,7 @@
 import os
 import tempfile
 
-__version__ = '2.46.2'
+__version__ = '2.46.3'
 
 __TEMP_STORAGE__ = os.path.join(tempfile.gettempdir(), 'carolina/cache')
 
@@ -48,3 +48,5 @@ from .cds import CDSGolden, CDSStaging
 from .apps import Apps
 from .subscription import Subscription
 from .carol_api import CarolAPI
+from . import bigquery
+from .sql import SQL
