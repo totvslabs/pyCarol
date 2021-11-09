a2cb91dfb4d17263f44d3fadb0f2007c25554f4b
felipebmendes <fmendes@totvs.com.br>
2021-11-08 23:52:29 -0300

    fix: renaming online app directories so they don't have whitespaces

--- tutorial/Online app api/main.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from app import create_app
-from flask_cors import CORS
-
-application = create_app()
-CORS(application)
-
-if __name__ == "__main__":
-    application.run(host='0.0.0.0', port=5000)
