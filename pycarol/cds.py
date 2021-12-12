1c4fd8686ef9627bc27baf550f9e9a525a2fe10f
rafarui <rafarui@gmail.com>
2021-12-12 15:01:46 +0100

    process via bigquery

--- pycarol/cds.py
+++ pycarol/cds.py
@@ -33,12 +33,14 @@ _MACHINE_FLAVORS = [
     'n2-highmem-64',
 ]
 
+
 def check_worker_type(worker_type):
     if worker_type not in _MACHINE_FLAVORS and worker_type is not None:
         warnings.warn(
-        f'worker_type {worker_type} is unknown. It might not work. Use one of the known machines {_MACHINE_FLAVORS}',
-        Warning, stacklevel=3
-    )
+            f'worker_type {worker_type} is unknown. It might not work. Use one of the known machines {_MACHINE_FLAVORS}',
+            Warning, stacklevel=3
+        )
+
 
 class CDSStaging:
     """
@@ -136,7 +138,7 @@ class CDSStaging:
             "forceDataflow": force_dataflow,
             "recursiveProcessing": recursive_processing,
             "rejectedType": dm_name, "autoScaling": auto_scaling,
-            "forcePaused": force_paused, 
+            "forcePaused": force_paused,
         }
 
         return self.carol.call_api(path='v1/cds/staging/processData', method='POST', params=query_params,
@@ -553,3 +555,55 @@ class CDSGolden:
         }
 
         return self.carol.call_api(path='v1/cds/golden/consolidate', method='POST', params=query_params)
+
+    def process_bigquery(
+        self, query, dm_name=None, dm_id=None,
+        save_cds_data=True, delete_target_folder=False,
+        send_subscriptions=True, use_dataflow=False, delete_realtime_records=False,
+        send_realtime_records=False,
+    ):
+        """
+        Process CDS using bigquery engine.
+
+        Args:
+            query: `str`
+                BigQuery query.
+            dm_name: `str`,
+                Data Model name.
+            dm_id: `str`, default `None`
+                Data Model id.
+            save_cds_data: `bool`, default `True`
+                Save result in CDS.
+            delete_target_folder: `bool`, default `False`
+                Delete target folder.
+            send_subscriptions: `bool`, default `True`  
+                Send subscriptions.
+            use_dataflow: `bool`, default `False`
+                Use Dataflow.
+            delete_realtime_records: `bool`, default `False`
+                Delete realtime records.
+            send_realtime_records: `bool`, default `False`
+                Send realtime records.
+
+        :return: `dict`
+            Task created in Carol. 
+        """
+
+        if dm_id:
+            dm_name = DataModel(self.carol).get_by_id(dm_id)['mdmName']
+        else:
+            if dm_name is None:
+                raise ValueError('dm_name or dm_id should be set.')
+
+        query_params = {
+            'entityTemplateName': dm_name,
+            'saveCds': save_cds_data,
+            'clearCds': delete_target_folder,
+            'sendSubscriptions': send_subscriptions,
+            'clearRealtime': delete_realtime_records,
+            'sendRealtime': send_realtime_records,
+            'useDataflow': use_dataflow,
+        }
+        extra_headers = {'Content-Type': 'text/plain'}
+
+        return self.carol.call_api(path='v2/bigQuery/processQuery', method='POST', data=query, params=query_params, extra_headers=extra_headers)
