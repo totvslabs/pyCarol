8bd49d851a4208374d658b082dee687da2f0b963
Jean Da Rolt <jeandarolt@gmail.com>
2022-11-15 19:00:57 -0300

    formatting code and adding new custom exceptions

--- pycarol/exceptions.py
+++ pycarol/exceptions.py
@@ -1,16 +1,31 @@
 class CarolApiResponseException(Exception):
-    """Custom Exception to handle Exception on Carol API Calls
 
-    Args:
-        Exception (Exception): SuperClass
-    """
+    """Custom Exception to handle Exception on Carol API Calls."""
+
     pass
+
 
 class InvalidToken(Exception):
 
-    """Custom Exception to handle Invalid token
-    Args:
-        Exception (Exception): SuperClass
-    """
+    """Custom Exception to handle Invalid token."""
+
     pass
 
+
+class MissingInfoCarolException(ValueError):
+
+    """When some information is missing to Carol.__init__."""
+
+    pass
+
+
+class DeprecatedEnvVarException(Exception):
+
+    """When some environment variable is deprecated."""
+
+    def __init__(self, old_var: str, new_var: str):
+        msg = f"""
+            {old_var} environment var is deprecated.
+            Please replace it by {new_var}.
+        """
+        super().__init__(msg)
