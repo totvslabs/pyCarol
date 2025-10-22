de6d72825ce31ff2372e04a983ee49a6b8621070
brenopapa <bpapa@outlook.com.br>
2025-10-22 15:21:00 -0300

    [Added] Correct fallback on unauthorized error

--- pycarol/carol.py
+++ pycarol/carol.py
@@ -333,6 +333,7 @@ class Carol:
                 if response.json().get("possibleResponsibleField") in [
                     "password",
                     "userLogin",
+                    "userLogin/password"
                 ]:
                     raise exceptions.InvalidToken(response.text)
                 self.auth.get_access_token()  # It will refresh token if Unauthorized
